ad3ee663b6de93b2344c1ec0f060f7b391c71365
AlejandroAmayaGonzalez <132985108+AlejandroAmayaGonzalez@users.noreply.github.com>
2024-10-02 17:34:26 +0200

    Add files via upload

---
+++ DI_T1A1_Ej2-7_AmayaAlejandro/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module ieslosmontecillos.di_t1a1_ej2_7_amayaalejandro {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+
+
+    opens ieslosmontecillos.di_t1a1_ej2_7_amayaalejandro to javafx.fxml;
+    exports ieslosmontecillos.di_t1a1_ej2_7_amayaalejandro;
+}
